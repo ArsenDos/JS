57f27ed6ccc2f8286dbdac4d51282bd9ef174e6a
Arsen <dosarsen112@gmail.com>
2023-12-08 23:52:37 +0600

    change ignore

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,35 @@
 package org.example;
 
+import javax.xml.transform.Result;
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ClassNotFoundException {
+        String url = "jdbc:mysql://localhost:3306";
+
+        String name = "root";
+        String pass = "root";
 
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        try (Connection connection = DriverManager.getConnection(url , name , pass)) {
+
+            Statement statement = connection.createStatement();
+            statement.execute("insert into test1(name) value(Arsen)");
+            ResultSet resultSet = statement.getResultSet();
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+
+
+
+            } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
 
 
     }
-}
+
+    }
