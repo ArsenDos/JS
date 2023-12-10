f4466403295bbcf7d6c6a709dc201bdccbd67c0c
Arsen <dosarsen112@gmail.com>
2023-12-10 23:26:26 +0600

    change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,23 @@
 package org.example;
 
-import javax.xml.transform.Result;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
-        String url = "jdbc:mysql://localhost:3306";
-
+        String url = "jdbc:mysql://localhost:3306/";
         String name = "root";
         String pass = "root";
-
-
         Class.forName("com.mysql.cj.jdbc.Driver");
-
-        try (Connection connection = DriverManager.getConnection(url , name , pass)) {
-
-            Statement statement = connection.createStatement();
-            statement.execute("insert into test1(name) value(Arsen)");
-            ResultSet resultSet = statement.getResultSet();
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("name"));
-            }
+        try (Connection connection = DriverManager.getConnection(url, name, pass)) {
 
 
-
-            } catch (SQLException ex) {
+        } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
 
-
     }
+}
 
-    }
